10dca5402d43e137e8062fb5e499c5b5ebc80e5d
Gres <gres@gres.biz>
2020-01-26 19:33:23 +0300

    Move config parsing to separate function

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,50 @@
 #include "manager.h"
 
 #include <QApplication>
-#include <QDebug>
 #include <QCommandLineParser>
-#include <QCommandLineOption>
+#include <QDebug>
+#include <QLibraryInfo>
+#include <QLockFile>
+#include <QStandardPaths>
+#include <QTranslator>
 
 #define STR2(XXX) #XXX
 #define STR(XXX) STR2(XXX)
 
+struct Cmd
+{
+  QString configFile{":/settings.ini"};
+};
+
+Cmd parseCmdline()
+{
+  QCommandLineParser parser;
+  parser.setApplicationDescription(QObject::tr("Screen time limiter tool"));
+  parser.addHelpOption();
+  parser.addVersionOption();
+
+  Cmd result;
+  const QCommandLineOption configOption(
+    {"c", "config"}, QStringLiteral("Configuration file to use."),
+    QStringLiteral("config"), result.configFile);
+  parser.addOption(configOption);
+
+  parser.process(QCoreApplication::arguments());
+
+  result.configFile = parser.value(configOption);
+  return result;
+}
+
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
-  QCoreApplication::setOrganizationName(QStringLiteral("Gres"));
-  QCoreApplication::setApplicationName(QStringLiteral("Screen Dose"));
-  QCoreApplication::setApplicationVersion(STR(VERSION_STRING));
+  a.setOrganizationName(QStringLiteral("Gres"));
+  a.setApplicationName(QStringLiteral("Screen Dose"));
+  a.setApplicationVersion(STR(VERSION_STRING));
 
-  QString configName = QStringLiteral(":/settings.ini");
-  {
-    QCommandLineParser parser;
-    parser.setApplicationDescription(QStringLiteral("Screen time limiter tool"));
-    parser.addHelpOption();
-    parser.addVersionOption();
+  const auto cmd = parseCmdline();
 
-    const QCommandLineOption configOption(
-      {"c", "config"}, QStringLiteral("Configuration file to use."),
-      QStringLiteral("config"), configName);
-    parser.addOption(configOption);
-
-    parser.process(QCoreApplication::arguments());
-
-    configName = parser.value(configOption);
-  }
-
-
-  Manager m(configName);
+  Manager m(cmd.configFile);
 
   return a.exec();
 }
